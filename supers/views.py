7431233040eb30d72792e0838c052b74014ef381
Colleen Stewart <collstewart30@gmail.com>
2022-08-29 22:34:18 -0400

    Started First Supers Views GET Function

--- supers/views.py
+++ supers/views.py
@@ -4,3 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
+
+
+@api_view(['GET'])
+def supers_list(request):
+    
+
+    return Response('ok')
