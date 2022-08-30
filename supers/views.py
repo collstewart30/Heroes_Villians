c82c2ceab6485f1a0a22fa52322a57af2078b2e1
Colleen Stewart <collstewart30@gmail.com>
2022-08-29 22:19:26 -0400

    Updated Imports Supers Views File

--- supers/views.py
+++ supers/views.py
@@ -1,2 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
