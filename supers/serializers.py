07c44d865600e309f7c00d587c26186f922b73f1
Colleen Stewart <collstewart30@gmail.com>
2022-08-29 22:10:14 -0400

    Updated Supers Serializer and Model

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,4 +4,7 @@ from .models import Super
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fiels = []
+        fiels = ['id','name','alter_ego','primary_ability','secondary_ability','catchphrase','super_type']
+        depth = 1
+
+    super_type_id = serializers.IntegerField(write_only=True)
