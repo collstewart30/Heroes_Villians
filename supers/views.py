f7ebc6137318b8f64f48afc66aea762dedc35df4
Colleen Stewart <collstewart30@gmail.com>
2022-08-31 14:50:04 -0400

    Completed Supers App Views File, Added Capability for Query Paramters, Re-Added Null=True for SuperSerializer

--- supers/views.py
+++ supers/views.py
@@ -9,11 +9,16 @@ from .models import Super
 @api_view(['GET', 'POST'])
 def supers_list(request):
     
-
-
     if request.method == "GET":
-        super = Super.objects.all()
-        serializer = SuperSerializer(super, many=True)    
+
+        super_type_name = request.query_params.get('super_type')        
+
+        queryset = Super.objects.all()
+
+        if super_type_name:
+            queryset = queryset.filter(super_type__name=super_type_name)
+
+        serializer = SuperSerializer(queryset, many=True)    
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
@@ -21,23 +26,16 @@ def supers_list(request):
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
 
-    super = get_object_or_404(super, pk=pk)
+    super = get_object_or_404(Super, pk=pk)
 
     if request.method == 'GET':
-
-        super_type_name = request.query_params.get('super_type')
-        print(super_type_name)
-
-        queryset = Super.objects.all()
         serializer = SuperSerializer(super)
-
-
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = SuperSerializer(super, data=request.data)
