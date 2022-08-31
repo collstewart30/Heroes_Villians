d5d1beb1b81b063fd7e80f95314ba50f6a1c6951
Colleen Stewart <collstewart30@gmail.com>
2022-08-30 20:57:27 -0400

    Registered Super Model with Admin Site

--- supers/views.py
+++ supers/views.py
@@ -9,5 +9,9 @@ from .models import Super
 @api_view(['GET'])
 def supers_list(request):
     
+    supers = Super.objects.all()
 
-    return Response('ok')
+    serializer = SuperSerializer(supers, many=True)
+
+   
+    return Response(serializer.data)
